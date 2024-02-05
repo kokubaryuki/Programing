bebc97ddc9792aed8d3307b9548436acbbdc9299
kokubaryuki <138412750+kokubaryuki@users.noreply.github.com>
2024-02-05 14:42:02 +0900

    kopipe

--- Game/DriveAndAvoid/Scene/RankingDispScene.h
+++ Game/DriveAndAvoid/Scene/RankingDispScene.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include"SceneBase.h"
+#include"../Object/RankingDate.h"
+
+class RankingDispScene:public SceneBase
+{
+private:
+
+private:
+	int background_image;
+	RankingData* ranking;
+
+
+public:
+	RankingDispScene();
+	virtual ~RankingDispScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eSceneType GetNowScene() const override;
+};
