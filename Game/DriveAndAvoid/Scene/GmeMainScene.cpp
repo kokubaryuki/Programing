4eb8fd2be7f870b9c4fc1700ff90f9437d978335
kokubaryuki <138412750+kokubaryuki@users.noreply.github.com>
2024-02-05 14:46:34 +0900

    a
    a

--- Game/DriveAndAvoid/Scene/GmeMainScene.cpp
+++ Game/DriveAndAvoid/Scene/GmeMainScene.cpp
@@ -2,7 +2,7 @@
 #include"../Object/RankingDate.h"
 #include"DxLib.h"
 #include<math.h>
-
+//
 GameMainScene::GameMainScene() :high_score(0), back_ground(NULL),
 barrier_image(NULL), mileage(0), player(nullptr), enemy(nullptr)
 {
